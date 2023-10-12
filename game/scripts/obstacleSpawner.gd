9301420e3590627681b5e2317cdf76ef7320f01b
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-10-11 22:08:31 -0400

    more spawners

---
+++ game/scripts/obstacleSpawner.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@export var obstacle_scene: PackedScene
+@export var obstacle_speed = 500
+
+enum SpawnLocations {
+	TOP,
+	MIDDLE,
+	BOTTOM
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ObstacleSpawnTimer.start()
+
+func _on_obstacle_spawn_timer_timeout():
+	var obstacle = obstacle_scene.instantiate()
+	var obstacle_sprite = obstacle.get_node("AnimatedSprite2D")
+	var spawn_location = $BottomSpawner
+	
+	var random_spawner = randi() % 3
+	if random_spawner == SpawnLocations.BOTTOM:
+		# Originally it was just randi() % 2 and if it's == 0 or 1
+		# but for some reason you'd sometimes just only get one
+		# sprite so changed to a larger range
+		var crate_or_spike = randi() % 10
+		if crate_or_spike >= 5:
+			obstacle_sprite.animation = "crate"
+		else:
+			obstacle_sprite.animation = "spike"
+	elif random_spawner == SpawnLocations.MIDDLE:
+		spawn_location = $MiddleSpawner
+		obstacle_sprite.animation = "bird"
+	else:
+		spawn_location = $TopSpawner
+		obstacle_sprite.animation = "bird"
+		
+	obstacle_sprite.flip_h = true
+		
+	obstacle.position = spawn_location.position
+	obstacle.linear_velocity = Vector2(-obstacle_speed, 0)
+	
+	add_child(obstacle)
--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -10,17 +10,20 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	var sprite = $AnimatedSprite2D
+	
 	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
+		sprite.flip_h = false
 	elif Input.is_action_pressed("move_left"):
 		velocity.x = -speed
+		sprite.flip_h = true
 		
 	if Input.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y = -jump_strength
 
-	var sprite = $AnimatedSprite2D
 	if velocity.x == 0:
 		sprite.animation = "idle"
 		sprite.play()
@@ -30,3 +33,12 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	move_and_slide()
 	
+func _on_area_2d_body_entered(body):
+	# This is because of some janky stuff I did with the nodes for
+	# the player scene. It's a CharacterBody2D which makes dealing
+	# with movements super easy but hard to detect collisions so I
+	# had to add an Area2D as a child with it's own collision.
+	if body.get_name() == "Player":
+		return
+	get_tree().change_scene_to_file("res://scenes/title.tscn")
+		
