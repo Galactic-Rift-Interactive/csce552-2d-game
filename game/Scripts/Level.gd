59c041bee3812989db98200458fb3b979a89d5d7
Deja S. <deja28.14@gmail.com>
2023-10-13 17:42:24 -0400

    Add: Added a stopwatch to the game.

--- game/Scripts/Level.gd
+++ game/Scripts/Level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var score = 0
+@onready var game_start_timer = Time.get_ticks_msec()
 
 func _ready():
 	$ScoreTimer.start()
@@ -18,3 +19,24 @@ func _on_player_hit():
 		GameManager.save() # TODO: Maybe move this somewhere else to not constantly save
 	# TODO: show game over menu
 	get_tree().change_scene_to_file("res://Scenes/title.tscn")
+
+func _process(delta):
+	$stopWatch.text = get_time()
+
+func get_time():
+	var current_time = Time.get_ticks_msec() - game_start_timer
+	var mins = current_time / 1000 / 60
+	var secs = current_time / 1000 % 60
+	var msec = current_time % 1000 / 10
+	
+	if mins < 10:
+		mins = "0" + str(mins)
+	if secs < 10:
+		secs = "0" + str(secs)
+	if msec < 10:
+		if msec == 0:
+			msec = "00"
+		else:
+			msec = "0" + str(msec)
+	
+	return str(mins) + ":" + str(secs) + ":" + str(msec)
